94fdabfee9ec17e4f7c78e154160ed4975cd8212
goraegori <gorae02@gmail.com>
2021-02-28 12:19:42 +0900

    rename output

---
+++ docs/precache-manifest.51dd79ad523d6aca5aa5b50a59f5fdef.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2856509333e250f78b82",
+    "url": "/css/app.d479248f.css"
+  },
+  {
+    "revision": "601a0dfa09253b95985d",
+    "url": "/css/chunk-vendors.6abdccf7.css"
+  },
+  {
+    "revision": "630835e5e8901b3c635d58ee3a67e81e",
+    "url": "/img/img_bg_user_soldier_sub.630835e5.png"
+  },
+  {
+    "revision": "7faf8b1ab3c36c01df540cf2dd29af42",
+    "url": "/img/img_common_mnsa_logo.7faf8b1a.png"
+  },
+  {
+    "revision": "a920393f31388694d936435b87e96dc1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84913a65295fede8b681",
+    "url": "/js/about.06ed02c9.js"
+  },
+  {
+    "revision": "2856509333e250f78b82",
+    "url": "/js/app.1dfa9ad7.js"
+  },
+  {
+    "revision": "601a0dfa09253b95985d",
+    "url": "/js/chunk-vendors.7e129a20.js"
+  },
+  {
+    "revision": "046d05460296f19f9393fbda69282841",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
